cfa77907be9b486e84c7d59fb7be2f09dbb539c1
Shakshi3104 <watchosastrom3104@gmail.com>
2021-04-20 10:48:19 +0900

    Initial Commit

---
+++ DepthMap/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  DepthMap
+//
+//  Created by MacBook Pro M1 on 2021/04/20.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
